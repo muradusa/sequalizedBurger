26122218459e48eb725404d33a0e127c3c6b8582
Murad Cholukov <mcholukov@welyons.local>
2020-04-07 08:06:12 -0700

    server.js half way done

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-var express = requare("express");
+var express = require("express");
+
+var db = require("./models");
+
+var app = express();
+
+app.use(express.static("public"));
+
+//Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
