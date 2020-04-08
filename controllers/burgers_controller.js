41274a250a6da2a8d2900864c3bf13706c0394e9
Murad Cholukov <mcholukov@welyons.local>
2020-04-08 07:37:13 -0700

    everything is ready except config folder

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,40 @@
+var express = require("express");
+
+var router = express.Router();
+
+var db = require("../models/");
+
+router.get("/", function (req, res) {
+    res.redirect("/burgers");
+});
+
+router.get("/burgers", function(req, res) {
+    db.Burger.findAll().then(function(dbBurger) {
+        console.log(dbBurger);
+        //into the main index, updating the page
+        var hbsObject = { burger: dbBurger };
+        return res.render("index", hbsObject);
+    });
+});
+
+router.post("/burgers/create", function(req,res) {
+    db.Burger.create({
+        burger_name: req.body.burger_name
+    }).then(function(dbBurger) {
+        console.log(dbBurger);
+        res.redirect("/");
+    });
+});
+
+router.put("/burgers/update/:id", function (req, res) {
+    db.Burger.update({
+        devoured: true
+    },
+    {
+        where: {id: req.params.id}
+    }).then(function(dbBurger) {
+        res.json("/")
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -11,3 +11,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller");
+
+app.use(routes);
+
+var PORT = process.env.PORT || 3000;
+db.sequelize.sync().then(function(){
+    app.listen(PORT, function(){
+        console.log("App now listening on port: ", PORT);
+    });
+});
