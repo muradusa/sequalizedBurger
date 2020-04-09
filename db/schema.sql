f3c3995a2e42dc0039b07ed9d1df6243d5fe84d1
Murad Cholukov <muradusa@gmail.com>
2020-04-09 07:10:26 -0700

    updated schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,14 @@
-CREATE DATABASE IF NOT EXISTS burgers_db;
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
 USE burgers_db;
 
 DROP TABLE IF EXISTS burgers;
 
 CREATE TABLE burgers (
-    id INT NOT NULL AUTO INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT, 
     burger_name VARCHAR(255) NOT NULL,
     devoured BOOLEAN DEFAULT FALSE,
     PRIMARY KEY (id)
 );
+
+SELECT * FROM burgers_db;
